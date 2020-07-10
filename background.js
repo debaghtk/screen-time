0f6ae858312257ae96471f927fce9996ef5c73f4
wallydrag <debashish.ghatak@gmail.com>
2020-07-11 04:48:04 +0530

    opens the html in a new tab instead of a popup when extension icon is clicked

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-chrome.tabs.onActivated.addListener(function(tabId, moveInfo) {
-  chrome.tabs.query({active: true}, function(tabs) {
-    const url = new URL(tabs[0].url)
-    chrome.storage.sync.set({hostname: url.origin, startTime: Date.now()}, function(){
-    });
-  });
+chrome.browserAction.onClicked.addListener(function(){
+    chrome.tabs.create({ 'url': "hello.html" });
 });
