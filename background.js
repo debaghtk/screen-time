24317ff320fe261ce485561847bbaaa5024cd28c
wallydrag <debashish.ghatak@gmail.com>
2020-07-11 21:35:46 +0530

    Fetches all tabs hostnames and prints in console

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
 chrome.browserAction.onClicked.addListener(function(){
-    chrome.tabs.create({ 'url': "hello.html" });
+	chrome.tabs.create({ 'url':"hello.html" });
+	console.log(chrome.tabs)
+	chrome.tabs.getAllInWindow(function(tabs) {
+		for (let step = 0; step < tabs.length; step++) {
+			console.log(new URL(tabs[step].url).host)
+		}
+	})
 });
